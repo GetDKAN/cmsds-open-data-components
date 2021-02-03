6b1c133a175030d0fa324b2a28569e5eb30a5ff2
dgading <dgading@gmail.com>
2021-02-03 15:47:47 -0500

    Add DatasetSearchListItem tests

---
+++ src/components/DatasetSearchListItem/datasetsearchlistitem.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen, within } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import DatasetSearchListItem from './index';
+
+const singleItem = {
+  title: "Dataset Title",
+  modified: "2020-10-22",
+  description: "This is my description.",
+  theme: ["dkan"],
+  keyword: ["my keyword"]
+}
+
+describe('<DatasetSearchListItem />', () => {
+  test('Renders correctly', () => {
+    render(<DatasetSearchListItem item={singleItem} />);
+    const listItemOptions = singleItem.theme.concat(singleItem.keyword)
+    const listItems = screen.getAllByRole('listitem')
+    listItems.forEach((item, idx) => {
+      const { getByText } = within(item);
+      expect(getByText(listItemOptions[idx])).toBeInTheDocument();
+    })
+    
+
+    expect(screen.getByRole('heading', { name: "Dataset Title" })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: "Dataset Title" })).toBeInTheDocument();
+    expect(screen.getByText('Updated October 22, 2020')).toBeInTheDocument();
+    expect(screen.getByText('This is my description.')).toBeInTheDocument();
+  });
+});
