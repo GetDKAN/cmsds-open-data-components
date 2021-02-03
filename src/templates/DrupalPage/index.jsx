24a86ab2c1223a389f8bff5cbcfbcdcccc5c84d7
Brian Seek <brian.seek@civicactions.com>
2021-02-03 14:15:57 -0800

    Added Drupal content page template. (#4)
    * Added Drupal content page template.
    
    * Added CMSDS container class.

---
+++ src/templates/DrupalPage/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useDrupalEntity } from '@civicactions/data-catalog-services';
+
+const DrupalPage = ({ path, rootURL, uuid, entityType, bundle, options }) => {
+  const {entity} = useDrupalEntity(rootURL, uuid, entityType, bundle, options);
+  const bodyContent = Object.keys(entity).length ? (
+    <div className="dc-page ds-l-container">
+      <h1>{entity.attributes.title}</h1>
+      <div className="dc-page-content" dangerouslySetInnerHTML={{ __html: entity.attributes.body.processed }} />
+    </div>
+  ) : '';
+    return (
+      <>
+      {bodyContent}
+      </>
+  );
+}
+export default DrupalPage;
