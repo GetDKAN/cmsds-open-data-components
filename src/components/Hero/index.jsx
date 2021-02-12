ca8cc6347fb446f523a4593857b7d3b7f6320d90
Brian Seek <brian.seek@civicactions.com>
2021-02-11 16:44:15 -0800

    Updates for hero.

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -6,18 +6,20 @@ const Hero = ({title, description, searchUrl, inverse}) => {
   return (
     <section className={`dc-c-hero ds-base ds-u-padding--2`}>
       <div className="ds-l-container">
-        <h1 class="ds-u-font-size--title">{title}</h1>
-        <p>{description}</p>
-        <div className="ds-l-form-row ds-u-align-items--stretch">
+        <div className="ds-l-row">
+          <h1 className="ds-u-font-size--display ds-u-color--white ds-u-margin-y--1">{title}</h1>
+          <p className="ds-u-color--white ds-u-measure--wide">{description}</p>
+        </div>
+        <div className="ds-l-row ds-u-align-items--stretch ds-u-margin-y--4 ds-u-md-flex-wrap--nowrap ds-u-flex-wrap--wrap">
           <TextField
             label="Search by physician, teaching hospital, or company name"
             labelClassName="ds-u-visibility--screen-reader"
             name="search_text_input"
-            className="ds-l-col--10"
+            className="ds-l-col--12 ds-l-md-col--10 ds-u-padding--0 ds-u-md-margin-right--1"
             onChange={e => setSearchValue(e.target.value)}
-            style={{maxWidth: "none", height: "100%", margin: "0 20px 0 0"}}
+            style={{maxWidth: "none", height: "61px", margin: "0 20px 0 0"}}
           />
-          <Button href={`${searchUrl}?search=${searchValue}`} className="ds-u-margin-left--auto ds-l-col--2" variation="default" size="big">
+          <Button href={`${searchUrl}?search=${searchValue}`} className="ds-u-margin-left--auto ds-l-col--12 ds-l-md-col--auto ds-u-margin-top--2 ds-u-md-margin-top--0" variation="default" size="big">
             Search
           </Button>
         </div>
