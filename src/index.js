ab892f7e9a0a00e2db47b7ccd63eb96ce1a4ee7b
Brian Seek <brian.seek@civicactions.com>
2021-04-08 08:32:45 -0700

    Added breadcrumb component. (#16)

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ export { default as ApiDocumentation } from './components/ApiDocumentation';
 
 export { default as ResourceConditionField } from './components/ResourceConditionField';
 export { default as ResourceFilter } from './components/ResourceFilter';
+export { default as Breadcrumb } from './components/Breadcrumb';
 
 // Templates
 export { default as Footer } from './templates/Footer';
