cdf5726db1b5665dfc3de8df719d9736a513eb5f
dgading <dgading@gmail.com>
2021-06-02 11:32:24 -0400

    Fix error when optional additional information is not included with dataset

--- src/components/DatasetAdditionalInformation/index.jsx
+++ src/components/DatasetAdditionalInformation/index.jsx
@@ -44,54 +44,102 @@ const DatasetAdditionalInformation = ({ datasetInfo }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <TableCell>Last Update</TableCell>
-            <TableCell><TransformedDate date={modified} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Issued</TableCell>
-            <TableCell><TransformedDate date={issued} /></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Publisher</TableCell>
-            <TableCell>{publisher.data.name}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Identifier</TableCell>
-            <TableCell>{identifier}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Contact</TableCell>
-            <TableCell>{contactPoint.fn}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Contact Email</TableCell>
-            <TableCell>{contactPoint.hasEmail}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Bureau Code</TableCell>
-            <TableCell>{bureauCode[0]}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Program Code</TableCell>
-            <TableCell>{programCode[0]}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Category</TableCell>
-            <TableCell>{themes.map(t => t.data).join(',')}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Tags</TableCell>
-            <TableCell>{tags}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>License</TableCell>
-            <TableCell><a href={license} target="_blank" rel="nofollow">{license}</a></TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Public Access Level</TableCell>
-            <TableCell>{accessLevel}</TableCell>
-          </TableRow>
+          {modified
+            &&(
+              <TableRow>
+                <TableCell>Last Update</TableCell>
+                <TableCell><TransformedDate date={modified} /></TableCell>
+              </TableRow>
+            )
+          }
+          {issued
+            && (
+              <TableRow>
+                <TableCell>Issued</TableCell>
+                <TableCell><TransformedDate date={issued} /></TableCell>
+              </TableRow>
+            )
+          }
+          {(publisher && publisher.data)
+            && (
+              <TableRow>
+                <TableCell>Publisher</TableCell>
+                <TableCell>{publisher.data.name}</TableCell>
+              </TableRow>
+            )
+          }
+          {identifier
+            && (
+              <TableRow>
+                <TableCell>Identifier</TableCell>
+                <TableCell>{identifier}</TableCell>
+              </TableRow>
+            )
+          }
+          {(contactPoint && contactPoint.fn)
+            && (
+              <TableRow>
+                <TableCell>Contact</TableCell>
+                <TableCell>{contactPoint.fn}</TableCell>
+              </TableRow>
+            )
+          }
+          {(contactPoint && contactPoint.hasEmail)
+            && (
+              <TableRow>
+                <TableCell>Contact Email</TableCell>
+                <TableCell>{contactPoint.hasEmail}</TableCell>
+              </TableRow>
+            )
+          }
+          {(bureauCode && bureauCode.length)
+            && (
+              <TableRow>
+                <TableCell>Bureau Code</TableCell>
+                <TableCell>{bureauCode[0]}</TableCell>
+              </TableRow>
+            )
+          }
+          {(programCode && programCode.length)
+            && (
+              <TableRow>
+                <TableCell>Program Code</TableCell>
+                <TableCell>{programCode[0]}</TableCell>
+              </TableRow>
+            )
+          }
+          {(themes && themes.length)
+            && (
+              <TableRow>
+                <TableCell>Category</TableCell>
+                <TableCell>{themes.map(t => t.data).join(',')}</TableCell>
+              </TableRow>
+            )
+          }
+          {(tags && tags.length)
+            && (
+              <TableRow>
+                <TableCell>Tags</TableCell>
+                <TableCell>{tags}</TableCell>
+              </TableRow>
+            )
+          }
+          {license
+            && (
+              <TableRow>
+                <TableCell>License</TableCell>
+                <TableCell><a href={license} target="_blank" rel="nofollow">{license}</a></TableCell>
+              </TableRow>
+            )
+          }
+          {accessLevel
+            && (
+              <TableRow>
+                <TableCell>Public Access Level</TableCell>
+                <TableCell>{accessLevel}</TableCell>
+              </TableRow>
+            )
+          }
         </TableBody>
       </Table>
     </div>
