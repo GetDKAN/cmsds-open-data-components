fcc79468c81e1d145e6ef3f61bc6b80d5168b716
Brian Seek <brian.seek@civicactions.com>
2021-06-07 09:38:30 -0700

    Added not found handling to dataset and filtered view. (#26)
    * Added not found handling to dataset and filtered view.
    
    * bump version.
    
    * Updated dep versions.

---
+++ src/templates/Dataset/DatasetBody.jsx
@@ -0,0 +1,104 @@
+import React, { useState, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import SwaggerUI from 'swagger-ui-react';
+import { useDatastore } from '@civicactions/data-catalog-services';
+import { Badge, Button } from '@cmsgov/design-system';
+import ResourcePreview from '../../components/ResourcePreview';
+import ResourceHeader from '../../components/ResourceHeader';
+import DatasetTags from '../../components/DatasetTags';
+import DatasetDownloads from '../../components/DatasetDownloads';
+import DatasetAdditionalInformation from '../../components/DatasetAdditionalInformation';
+import TransformedDate from '../../components/TransformedDate';
+import ResourceFooter from '../../components/ResourceFooter';
+
+const DatasetBody = ({ id, dataset }) => {
+  let apiDocs = useRef()
+  const [tablePadding, setTablePadding] = useState('ds-u-padding-y--1')
+
+  let distribution = {};
+  let distribution_array = dataset.distribution ? dataset.distribution : [];
+  if(distribution_array.length) {
+    distribution = distribution_array[0];
+  }
+  const resource = useDatastore('', process.env.REACT_APP_ROOT_URL, {
+    limit: 10,
+    manual: true,
+  })
+  useEffect(() => {
+    if(distribution.identifier) {
+      resource.setResource(distribution.identifier);
+      resource.setManual(false)
+    }
+  }, [distribution])
+
+  const rawDate = new Date(dataset.modified);
+  let modifiedDate = '';
+  let options = { year: 'numeric', month: 'long', day: 'numeric' };
+  if(rawDate) {
+    modifiedDate = rawDate.toLocaleDateString('en-US', options);
+  }
+  return (
+    <section className="ds-l-container">
+      <div className="ds-l-row ds-u-padding-top--3">
+        <div className="ds-l-md-col--9 ds-l-sm-col--12">
+          <h1 className="ds-title">{dataset.title}</h1>
+          <div className="ds-l-row">
+            <p className="ds-l-col--6">
+              {dataset.theme ? <Badge variation="info">{dataset.theme[0].data}</Badge> : null}
+            </p>
+            <p className="ds-l-col--6 ds-u-color--gray ds-u-text-align--right">Updated <TransformedDate date={modifiedDate} /></p>
+          </div>
+          <p dangerouslySetInnerHTML={{__html: dataset.description}} />
+          <h2 className="dc-resource-header">Resource Preview</h2>
+          {resource.columns
+              ? (
+                <div>
+                  <ResourceHeader id={id} includeFiltered includeDensity={true} setTablePadding={setTablePadding} distribution={distribution} resource={resource} />
+                  <ResourcePreview id={distribution.identifier} tablePadding={tablePadding} resource={resource} />
+                  <ResourceFooter resource={resource} />
+                </div>
+              )
+              : (
+                <Spinner />
+              )
+            }
+          {dataset.identifier &&
+            <DatasetAdditionalInformation datasetInfo={dataset} />
+          }
+          {dataset.identifier &&
+            <div ref={apiDocs}>
+              <h2>Try the API</h2>
+              <SwaggerUI url={`${process.env.REACT_APP_ROOT_URL}/metastore/schemas/dataset/items/${dataset.identifier}/docs`} docExpansion={'list'} />
+            </div>
+          }
+          
+        </div>
+        <div className="ds-l-md-col--3 ds-l-sm-col--12">
+        {dataset.distribution
+          && (
+            <DatasetDownloads downloadURL={dataset.distribution[0].data.downloadURL} />
+          )}
+          <DatasetTags keywords={dataset.keyword} />
+          <div className="dc-c-dataset-tags ds-u-margin-bottom--3 ds-u-padding--2 ds-u-border ds-u-border--1">
+            <h2 className="ds-u-color--primary ds-u-font-size--h3 ds-u-margin-top--0 ds-u-margin-bottom--2 ds-u-padding-bottom--2">API</h2>
+            <Button
+              variation="transparent"
+              onClick={
+                () => window.scrollTo({ behavior: 'smooth', top: apiDocs.current.offsetTop })
+              }
+              >
+                Scroll to dataset API
+              </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+DatasetBody.propTypes = {
+  id: PropTypes.string.isRequired,
+  dataset: PropTypes.object.isRequired,
+}
+
+export default DatasetBody;
--- src/templates/FilteredResource/index.jsx
+++ src/templates/FilteredResource/index.jsx
@@ -1,144 +1,46 @@
-import React, { useEffect, useRef } from 'react';
-import qs from 'qs';
-import { Link } from '@reach/router';
-import { ApiDocs } from '@civicactions/data-catalog-components';
-import { useMetastoreDataset, useDatastore } from '@civicactions/data-catalog-services';
-import { HelpDrawerToggle, Button, Tooltip, Spinner } from '@cmsgov/design-system';
-import ResourceFilter from '../../components/ResourceFilter';
-import ResourceHeader from '../../components/ResourceHeader';
-import ResourcePreview from '../../components/ResourcePreview';
-import ResourceFooter from '../../components/ResourceFooter';
+import React, {useState, useEffect} from 'react';
+import { useMetastoreDataset } from '@civicactions/data-catalog-services';
+import PageNotFound from '../PageNotFound';
+import FilteredResourceBody from './FilteredResourceBody';
 
 const FilteredResource = ({id, dist_id, location}) => {
-  const [tablePadding, setTablePadding] = React.useState('ds-u-padding-y--1')
-  let apiDocs = useRef();
-  const [filtersOpen, setFiltersOpen] = React.useState(false)
+  const [ready, setReady] = useState(false);
+  const [error, setError] = useState(false);
   const {dataset} = useMetastoreDataset(id, process.env.REACT_APP_ROOT_URL);
-  let distribution = {};
-  let distribution_array = dataset.distribution ? dataset.distribution : [];
-  if(distribution_array.length) {
-    distribution = distribution_array.find((dist) => dist.identifier === dist_id);
-  }
-  let buttonRef = null;
-  const options = location.search ? {...qs.parse(location.search)} : {conditions: []};
-  const resource = useDatastore('', process.env.REACT_APP_ROOT_URL, {
-    ...options,
-    limit: 25,
-    manual: true,
-  })
   useEffect(() => {
-    if(distribution.identifier) {
-      resource.setResource(distribution.identifier);
-      resource.setManual(false)
+    if (dataset.error) {
+      setError(true);
     }
-  }, [distribution])
-  const downloadUrl = `${process.env.REACT_APP_ROOT_URL}/datastore/query/${distribution.identifier}/download?${qs.stringify({conditions: resource.conditions}, { encode: false })}&format=csv`;
+    if (dataset.distribution && dataset.distribution.length) {
+      setReady(true);
+      if (!dataset.distribution.filter(dist => dist.identifier === dist_id).length > 0) {
+        setError(true);
+      }
+    }
+  }, [dataset.distribution, dataset.error, dist_id]);
+  const notFoundContent = (
+    <>
+      <h1>Error: Dataset not found</h1>
+      <p>We're sorry, but there is no dataset ID that matches your entry. You may have been directed here because:</p>
+      <ol>
+        <li>The address you typed contains a typo;</li>
+        <li>The requested dataset no longer exists.</li>
+      </ol>
+      <p><span className="ds-u-font-weight--bold">Note:</span> If you were using a bookmark, please reset it once you find the correct dataset.</p>
+    </>
+  );
 
   return (
-    <section className="ds-l-container ds-u-padding-bottom--3 ds-u-margin-bottom--2">
-      {Object.keys(distribution).length
-        && (
-          <>
-            <Link
-              to={`/dataset/${id}`}
-              className="ds-u-padding-y--3 ds-u-display--block"
-            >
-              Back to {dataset.title}
-            </Link>
-            <h1 className="ds-title">{distribution.data.title}</h1>
-            <p className="ds-u-margin-top--0" dangerouslySetInnerHTML={{__html: distribution.data.description}} />
-            <div className="ds-l-row ds-u-align-items--stretch">
-              <div className="ds-l-md-col--4 ds-l-sm-col--12 ds-u-margin-bottom--3">
-                <div className="dc-c-resource-action ds-u-border--1 ds-u-radius ds-u-display--flex ds-u-flex-direction--column ds-u-text-align--center">
-                  <h2 className="ds-u-color--primary ds-u-font-size--h3 ds-u-margin-bottom--2 ds-u-padding-bottom--0 ds-u-padding-left--3 ds-u-padding-left--3  ds-u-text-align--left">Create</h2>
-                  <div className="dc-filtered-resource-toggle">
-                    <HelpDrawerToggle
-                      helpDrawerOpen={filtersOpen}
-                      showDrawer={() => setFiltersOpen(!filtersOpen)}
-                      className="ds-u-text-decoration--none ds-u-font-weight--bold ds-c-button ds-c-button--primary ds-u-padding-y--1 ds-u-padding-x--3"
-                    >
-                      Filter data
-                    </HelpDrawerToggle>
-
-                  </div>
-                  
-                </div>
-              </div>
-              <div className="ds-l-md-col--4 ds-l-sm-col--12 ds-u-margin-bottom--3">
-                <div className="ds-u-border--1 ds-u-radius">
-                  <h2 className="ds-u-color--primary ds-u-font-size--h3 ds-u-margin-bottom--0 ds-u-padding-bottom--0 ds-u-padding-left--3">Access</h2>
-                  <Button
-                    variation="transparent"
-                    className="ds-u-text-align--left ds-u-font-weight--normal"
-                    href={downloadUrl}
-                  >
-                    Download filtered view (CSV)
-                  </Button>
-                  <Tooltip
-                    onOpen={() => {navigator.clipboard.writeText(window.location.href);}}
-                    className="ds-c-button ds-c-button--transparent ds-u-text-align--left"
-                    placement="bottom"
-                    dialog
-                    title="Link copied to clipboard"
-                  >
-                    Copy link to filtered view
-                  </Tooltip>
-                </div>
-              </div>
-              <div className="ds-l-md-col--4 ds-l-sm-col--12 ds-u-margin-bottom--3">
-                <div className=" ds-u-border--1 ds-u-radius">
-                  <h2 className="ds-u-color--primary ds-u-font-size--h3 ds-u-margin-y--0 ds-u-padding-bottom--0 ds-u-padding-left--3 ds-u-padding-left--3">Try API</h2>
-                  <Button
-                    variation="transparent"
-                    className="ds-u-text-align--left ds-u-font-weight--normal" 
-                    onClick={() => window.scrollTo({ behavior: 'smooth', top: apiDocs.current.offsetTop })}
-                  >
-                    Scroll to filtered view API
-                  </Button>
-                  <Button
-                    variation="transparent"
-                    className="ds-u-text-align--left ds-u-font-weight--normal"
-                  >
-                    View API documentation
-                  </Button>
-                </div>
-              </div>
-            </div>
-            {resource.columns
-              ? (
-                <div>
-                  <ResourceHeader includeDensity={true} setTablePadding={setTablePadding} distribution={distribution} resource={resource} />
-                  <ResourcePreview id={dist_id} tablePadding={tablePadding} resource={resource} />
-                  <ResourceFooter resource={resource} />
-                  {filtersOpen
-                    && (<ResourceFilter
-                          id={dist_id}
-                          resource={resource}
-                          filterOpen={filtersOpen}
-                          setFilterOpen={setFiltersOpen}
-                          helpDrawerButton={buttonRef}
-                        />)
-                  }
-                </div>
-                
-              )
-              : (
-                <Spinner />
-              )
-            }
-            {dataset.identifier &&
-              <div ref={apiDocs}>
-                <h2>Try out the API</h2>
-                <ApiDocs 
-                  endpoint={`${process.env.REACT_APP_ROOT_URL}`}
-                  datasetID={dataset.identifier}
-                />
-              </div>
-            }
-          </>
-        )
-      }
-    </section>
+    <>
+    { error ? (
+      <PageNotFound content={notFoundContent} />
+      ) : (
+        <>
+          { ready && (<FilteredResourceBody id={id} dataset={dataset} dist_id={dist_id} location={location} />)}
+        </>
+      )
+    }
+    </>
   )
 }
 
