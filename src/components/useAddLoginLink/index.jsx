7cafe4fca8b1948384d3d2932939c701a2bdb4f2
dgading <dgading@gmail.com>
2021-06-10 15:51:32 -0400

    Move hook from downstream to upstream

--- src/components/useAddLoginLink/index.jsx
+++ src/components/useAddLoginLink/index.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-const useAddLoginLink = (hosts, menuItems, loginLinkObj) => {
-  let linksArray = menuItems;
+const useAddLoginLink = (hosts, menuItems, targetKey, loginLinkObj) => {
+  const [linksArray, setLinksArray] = useState({...menuItems});
   useEffect(() => {
-    console.log('fired')
-  }, [])
+    let localLinks = {...menuItems}
+    if(hosts.findIndex((host) => host === window.location.host) > -1) {
+      if(localLinks[targetKey].findIndex((link) => link.id === loginLinkObj.id) === -1) {
+        localLinks[targetKey].unshift(loginLinkObj)
+        setLinksArray(localLinks);
+      }
+    }
+  }, [linksArray])
   return linksArray;
 }
 
