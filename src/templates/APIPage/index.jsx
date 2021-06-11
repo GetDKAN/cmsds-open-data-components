68d2c992ae7bc0f0d4a98499dfa849b012724b8f
Daniel <dgading@gmail.com>
2021-06-11 12:18:04 -0400

    Hide auth with prop on API page (#28)
    * Add auth back to /api
    
    * Hide auth on api page by prop

--- src/templates/APIPage/index.jsx
+++ src/templates/APIPage/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
 
-const APIPage = () => (
+const APIPage = ({hideAuth}) => (
   <section className="ds-l-container">
-    <SwaggerUI url={`${process.env.REACT_APP_ROOT_URL}?authentication=false`} docExpansion={'list'} />;
+    <SwaggerUI url={`${process.env.REACT_APP_ROOT_URL}${hideAuth ? '?authentication=false' : ''}`} docExpansion={'list'} />;
   </section>
 );
+
+APIPage.defaultProps = {
+  hideAuth: false,
+}
 
 export default APIPage;
