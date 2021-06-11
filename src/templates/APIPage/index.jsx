c89c5d23d616cb60ab96fa80eecd7db376c25c47
dgading <dgading@gmail.com>
2021-06-11 11:49:32 -0400

    Add auth back to /api

--- src/templates/APIPage/index.jsx
+++ src/templates/APIPage/index.jsx
@@ -3,7 +3,7 @@ import SwaggerUI from 'swagger-ui-react';
 
 const APIPage = () => (
   <section className="ds-l-container">
-    <SwaggerUI url={`${process.env.REACT_APP_ROOT_URL}?authentication=false`} docExpansion={'list'} />;
+    <SwaggerUI url={`${process.env.REACT_APP_ROOT_URL}`} docExpansion={'list'} />;
   </section>
 );
 
