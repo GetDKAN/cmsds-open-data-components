08e4132186fc8c4c4180c1452f3d04d51d2716fb
dgading <dgading@gmail.com>
2021-06-11 13:49:39 -0400

    Remove trailing ; from api page

--- src/templates/APIPage/index.jsx
+++ src/templates/APIPage/index.jsx
@@ -3,7 +3,7 @@ import SwaggerUI from 'swagger-ui-react';
 
 const APIPage = ({hideAuth}) => (
   <section className="ds-l-container">
-    <SwaggerUI url={`${process.env.REACT_APP_ROOT_URL}${hideAuth ? '?authentication=false' : ''}`} docExpansion={'list'} />;
+    <SwaggerUI url={`${process.env.REACT_APP_ROOT_URL}${hideAuth ? '?authentication=false' : ''}`} docExpansion={'list'} />
   </section>
 );
 
