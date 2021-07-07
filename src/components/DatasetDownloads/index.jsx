426962b2eaf474ef0468263f27022aed1ada095f
Daniel <dgading@gmail.com>
2021-07-07 14:45:43 -0400

    Add check for mediaType if no file format on dataset (#43)

--- src/components/DatasetDownloads/index.jsx
+++ src/components/DatasetDownloads/index.jsx
@@ -6,7 +6,7 @@ const DatasetDownloads = ({ downloadURL, type }) => {
     <div className="ds-u-margin-bottom--3 ds-u-padding--2 ds-u-border ds-u-border--1">
       <h2 className="ds-u-color--primary ds-u-font-size--h3 ds-u-margin-top--0 ds-u-margin-bottom--2 ds-u-padding-bottom--2 ds-u-border ds-u-border-bottom--1">Downloads</h2>
       <p className="ds-u-margin-bottom--1 ds-u-color--gray">Resource</p>
-      <a href={downloadURL}>Download this resource ({type.toUpperCase()})</a>
+      <a href={downloadURL}>Download this resource ({type})</a>
     </div>
   );
 }
