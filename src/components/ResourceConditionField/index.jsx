4102eef1be4c7653b77a17ddd5175e78ab6d504d
dgading <dgading@gmail.com>
2021-10-07 17:25:07 -0400

    Update to use mysql_type and force UTC time

--- src/components/ResourceConditionField/index.jsx
+++ src/components/ResourceConditionField/index.jsx
@@ -3,6 +3,16 @@ import { Button, Dropdown, TextField } from '@cmsgov/design-system';
 import DatePicker from "react-datepicker";
 import CloseIcon from '../../assets/icons/close';
 import "react-datepicker/dist/react-datepicker.css";
+
+function convertUTCToLocalDate(date) {
+  if (!date) {
+    return date
+  }
+  date = new Date(date)
+  date = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate())
+  return date
+}
+
 function buildOperatorOptions(type) {
   switch(type) {
     case 'text':
@@ -11,12 +21,15 @@ function buildOperatorOptions(type) {
         {label: "Contains", value: "like"},
         {label: "Is Not", value: "<>"},
         {label: "Or", value: "in"},
-        {label: "Date", value: "="},
-        {label: "Greater Than", value: ">"}
+        
       ]
     case 'date':
-      console.log('date')
-      return []
+      return [
+        {label: "Is", value: "="},
+        {label: "Is Not", value: "<>"},
+        {label: "Greater Than", value: ">"},
+        {label: "Less Than", value: "<"},
+      ]
     default:
       return []
   }
@@ -31,14 +44,18 @@ function cleanText(value, operator) {
 }
 
 const ResourceConditionField = ({data, index, remove, schema, update}) => {
-  const [startDate, setStartDate] = React.useState(data.value ? new Date(data.value) : new Date('2021-01-01 00:00:00'));
+  let myDate = new Date(data.value)
+  console.log(myDate)
+  myDate = new Date(Date.UTC(myDate.getFullYear(), myDate.getMonth(), myDate.getDate()))
+  const [startDate, setStartDate] = React.useState(data.value ? new Date(data.value) : new Date(Date.now()));
+  console.log(myDate, startDate, typeof myDate.getMonth === 'function')
   const { fields } = schema;
   let operatorOptions = []
   const columnOptions = Object.keys(fields).map((key) => { return {label: fields[key].description ? fields[key].description : key, value: key}});
   if(data.property) {
-    operatorOptions = buildOperatorOptions(fields[data.property].type).map((opt) => { return {label: opt.label, value: opt.value}});
+    operatorOptions = buildOperatorOptions(fields[data.property].mysql_type).map((opt) => { return {label: opt.label, value: opt.value}});
   }
-
+  console.log(fields)
   console.log(data)
   return(
     <fieldset>
@@ -72,17 +89,17 @@ const ResourceConditionField = ({data, index, remove, schema, update}) => {
       </div>
       <div className="ds-l-form-row">
         
-        {data.property && data.property.toLowerCase() === 'effective_date'
+        {data.property && fields[data.property].mysql_type.toLowerCase() === 'date'
           ? (
-            // <p>{startDate.getMonth()}</p>
             <div className="ds-u-clearfix ds-l-col--11">
               <DatePicker
-                selected={startDate}
+                selected={convertUTCToLocalDate(startDate)}
                 onChange={(date) => {setStartDate(date); update(index, 'value', date.toJSON().slice(0, 10))}}
                 showMonthDropdown
                 showYearDropdown
                 dropdownMode="select"
                 className="ds-c-field"
+                
               />
             </div>
             )
