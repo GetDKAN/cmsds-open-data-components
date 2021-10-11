dbe14e393e54d437e920b6e70ce173bb074ad28a
Adrienne <acabouet@protonmail.com>
2021-10-11 15:32:54 -0600

    Upstream fix to make link classes customizable for WCMS-5482.

--- src/templates/header/index.jsx
+++ src/templates/header/index.jsx
@@ -10,7 +10,7 @@ const Header = ({
   links,
   org,
   searchModalText,
-  customSearch, 
+  customSearch,
   includeTopNav,
   inversedModalButton,
   inversedSearchButton,
@@ -24,7 +24,7 @@ const Header = ({
             <div className="ds-l-sm-col--12 ds-l-lg-col--8">
               <div className="cms-link-container">
                 <a
-                  href={url} 
+                  href={url}
                   title={urlTitle}
                 >
                   <img src={logo} alt={logoAltText} />
@@ -57,7 +57,7 @@ const Header = ({
               menuName="CMS Site Main Nav"
               menuId="site"
               menuClasses="ds-u-display--flex dc-c-header--links ds-u-align-items--center"
-              linkClasses="ds-u-xl-margin-right--4 ds-u-margin-right--3 ds-u-padding-y--3"
+              linkClasses={linkClasses}
             />
             <div className="dc-c-main-navigation--search ds-u-margin-left--auto ds-u-lg-padding-left--1 ds-u-xl-padding-left--3">
               {customSearch ? (
@@ -84,6 +84,7 @@ Header.defaultProps = {
     urlTitle: 'CMS.gov Centers for Medicare & Medicaid Services',
     logo: cmsLogo,
     logoAltText: 'CMS.gov Centers for Medicare & Medicaid Services',
+    linkClasses: 'ds-u-xl-margin-right--4 ds-u-margin-right--3 ds-u-padding-y--3',
   },
   includeTopNav: true,
   customSearch: false,
