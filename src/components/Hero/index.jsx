a6649ea8631a56ab523fd339f40eeadd9cd72a8d
dgading <dgading@gmail.com>
2021-10-25 17:09:34 -0400

    Update the homepage hero to have a more responsive design like OpenPayments

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -14,24 +14,28 @@ const Hero = ({title, description, searchUrl, searchKey}) => {
     <section className={`dc-c-hero ds-base ds-u-padding--2`}>
       <div className="ds-l-container">
         <div className="ds-l-row">
-          <h1 className="ds-u-font-size--display ds-u-color--white ds-u-margin-y--1">{title}</h1>
+          <h1 className="ds-text-heading--5xl ds-u-color--white ds-u-margin-y--1">{title}</h1>
           <p className="ds-u-color--white ds-u-measure--wide">{description}</p>
         </div>
         <form
           onSubmit={(e) => submitHero(e)}
           className="ds-l-row ds-u-align-items--stretch ds-u-margin-y--4 ds-u-md-flex-wrap--nowrap ds-u-flex-wrap--wrap"
         >
-          <TextField
-            label="Search by physician, teaching hospital, or company name"
-            labelClassName="ds-u-visibility--screen-reader"
-            name="search_text_input"
-            className="ds-l-col--12 ds-l-md-col--10 ds-u-padding--0 ds-u-md-margin-right--1"
-            onChange={e => setSearchValue(e.target.value)}
-            style={{maxWidth: "none", height: "61px", margin: "0 20px 0 0"}}
-          />
-          <Button type="submit" className="ds-u-margin-left--auto ds-l-col--12 ds-l-md-col--auto ds-u-margin-top--2 ds-u-md-margin-top--0" variation="default" size="big">
-            Search
-          </Button>
+          <div style={{position: 'relative'}} className="ds-l-row ds-u-align-items--stretch ds-u-margin-y--4 ds-u-flex-wrap--nowrap">
+            <div className="ds-u-padding--0 ds-u-margin-right--1" style={{flex: "1 1 100%", maxWidth: "100%"}}>
+              <TextField
+                label="Search by physician, teaching hospital, or company name"
+                labelClassName="ds-u-visibility--screen-reader"
+                name="search_text_input"
+                style={{maxWidth: "none", height: "61px", margin: "0 20px 0 0"}}
+                onChange={e => setSearchValue(e.target.value)}
+              />
+            </div>
+            <Button className="ds-u-margin-left--auto" size="big" type="submit">
+              <span className="fas fa-search small-text ds-u-sm-display--none" />
+              <span className="full-text ds-u-display--none ds-u-sm-display--inline-block">Search</span>
+            </Button>
+          </div>
         </form>
       </div>
     </section>
