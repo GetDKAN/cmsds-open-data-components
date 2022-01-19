b3b6e80276ce0e1c106183911ee73fca744a6426
Daniel <dgading@gmail.com>
2022-01-19 10:34:12 -0500

    Update 404 titles (#69)

--- src/templates/Dataset/index.jsx
+++ src/templates/Dataset/index.jsx
@@ -1,52 +1,62 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useMetastoreDataset } from '@civicactions/data-catalog-services';
-import DatasetBody from './DatasetBody';
-import PageNotFound from '../PageNotFound';
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { useMetastoreDataset } from "@civicactions/data-catalog-services";
+import DatasetBody from "./DatasetBody";
+import PageNotFound from "../PageNotFound";
 
-
-const Dataset = ({ id, rootUrl, additionalParams, customColumns, setDatasetTitle }) => {
+const Dataset = ({
+  id,
+  rootUrl,
+  additionalParams,
+  customColumns,
+  setDatasetTitle,
+}) => {
   const metastore = useMetastoreDataset(id, rootUrl, additionalParams);
   const { dataset } = metastore;
   const title = dataset.title ? dataset.title : "";
   useEffect(() => {
-    if(title) {
-      if(setDatasetTitle) {
+    if (title) {
+      if (setDatasetTitle) {
         setDatasetTitle(title);
       }
     }
-  }, [title])
+  }, [title]);
   const notFoundContent = (
-    <>
-      <h1>Error: Dataset not found</h1>
-      <p>We're sorry, but there is no dataset ID that matches your entry. You may have been directed here because:</p>
+    <div className="ds-u-padding-top--3">
+      <h1 className="ds-title">Error: Dataset not found</h1>
+      <p>
+        We're sorry, but there is no dataset ID that matches your entry. You may
+        have been directed here because:
+      </p>
       <ol>
         <li>The address you typed contains a typo;</li>
         <li>The requested dataset no longer exists.</li>
       </ol>
-      <p><span className="ds-u-font-weight--bold">Note:</span> If you were using a bookmark, please reset it once you find the correct dataset.</p>
-    </>
+      <p>
+        <span className="ds-u-font-weight--bold">Note:</span> If you were using
+        a bookmark, please reset it once you find the correct dataset.
+      </p>
+    </div>
   );
   return (
     <>
-    { dataset.error ? (
-      <PageNotFound content={notFoundContent} />
+      {dataset.error ? (
+        <PageNotFound content={notFoundContent} />
       ) : (
-      <DatasetBody
-        id={id}
-        dataset={dataset}
-        additionalParams={additionalParams}
-        customColumns={customColumns ? customColumns : []}
-      />
-      )
-    }
+        <DatasetBody
+          id={id}
+          dataset={dataset}
+          additionalParams={additionalParams}
+          customColumns={customColumns ? customColumns : []}
+        />
+      )}
     </>
   );
-}
+};
 
 Dataset.propTypes = {
   id: PropTypes.string.isRequired,
   rootUrl: PropTypes.string.isRequired,
-}
+};
 
 export default Dataset;
--- src/templates/PageNotFound/index.jsx
+++ src/templates/PageNotFound/index.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const PageNotFound = ({siteUrl, content}) => {
+const PageNotFound = ({ siteUrl, content }) => {
   return (
-    <section className="ds-l-container">
-      {content
-        ? (
-          content
-        ) : (
-          <>
-            <h1>Error: Page not found</h1>
-            <p>We're sorry, but there is no {siteUrl} web page that matches your entry. You may have been directed here because:</p>
-            <ol>
-              <li>The address you typed contains a typo;</li>
-              <li>The requested page may have expired or;</li>
-              <li>The requested page may have been moved.</li>
-            </ol>
-            <p><span className="ds-u-font-weight--bold">Note:</span> If you were using a bookmark, please reset it once you find the correct page.</p>
-          </>
-        )
-      }
+    <section className="ds-l-container ds-u-padding-top--3">
+      {content ? (
+        content
+      ) : (
+        <>
+          <h1 className="ds-title">Error: Page not found</h1>
+          <p>
+            We're sorry, but there is no {siteUrl} web page that matches your
+            entry. You may have been directed here because:
+          </p>
+          <ol>
+            <li>The address you typed contains a typo;</li>
+            <li>The requested page may have expired or;</li>
+            <li>The requested page may have been moved.</li>
+          </ol>
+          <p>
+            <span className="ds-u-font-weight--bold">Note:</span> If you were
+            using a bookmark, please reset it once you find the correct page.
+          </p>
+        </>
+      )}
     </section>
-  )
-}
+  );
+};
 
 export default PageNotFound;
