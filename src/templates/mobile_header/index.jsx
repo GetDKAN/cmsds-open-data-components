9772a06b80cc065fd17edf837b59f2fa85ff6c90
dgading <dgading@gmail.com>
2022-02-01 10:34:22 -0500

    Remove console log from code

--- src/templates/mobile_header/index.jsx
+++ src/templates/mobile_header/index.jsx
@@ -143,7 +143,6 @@ const MobileHeader = ({
                 <SearchModal
                   searchFunc={(e) => {
                     e.preventDefault();
-                    console.log(e.target.value);
                   }}
                   searchModalText={searchModalText}
                   buttonSize={"small"}
