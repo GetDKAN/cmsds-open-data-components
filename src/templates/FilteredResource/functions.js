e3dbb330645d13dc773babc9a0d2cb2f6b0b4215
Daniel <dgading@gmail.com>
2022-03-02 10:29:46 -0500

    Update custom table headers to use api data and overwrite with custom

--- src/templates/FilteredResource/functions.js
+++ src/templates/FilteredResource/functions.js
@@ -1,8 +1,13 @@
 export function buildCustomColHeaders(customHeaders, columns, schema) {
   return columns.map((column) => {
-    const customHeaderIndex = customHeaders.findIndex((header) => header.id === column);
+    const customHeaderIndex = customHeaders.findIndex((header) => header.accessor === column);
     if (customHeaderIndex > -1) {
-      return customHeaders[customHeaderIndex];
+      return {
+        Header:
+          schema && schema.fields[column].description ? schema.fields[column].description : column,
+        accessor: column,
+        ...customHeaders[customHeaderIndex],
+      };
     }
     return {
       Header:
