00f1c9a0a5ee85383229cf4e80e1095b296605bf
Daniel <dgading@gmail.com>
2023-04-04 00:28:27 -0400

    Redo headers to fix sort and make easier to read

---
+++ src/components/Datatable/FixedSizeTHead.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  useReactTable,
+  flexRender,
+  getCoreRowModel,
+  createColumnHelper,
+  ColumnResizeMode,
+  ColumnDef,
+  getSortedRowModel,
+  SortingState,
+} from "@tanstack/react-table";
+
+const FixedSizeTHead = ({table, sortElement}) => {
+  return(
+    <thead className="dc-thead--fixed-size">
+      {table.getHeaderGroups().map(headerGroup => (
+        <tr key={headerGroup.id}>
+          {headerGroup.headers.map(header => {
+            return(
+              <th {
+                ...{
+                  key: header.id,
+                  style: {
+                    width: header.getSize(),
+                  },
+                  title: header.column.columnDef.header
+                }
+              }
+              className="ds-u-border-y--2 ds-u-padding--2 ds-u-border--dark  ds-u-font-weight--bold dc-c-table-header-cell"
+              >
+                <div onClick={header.column.getToggleSortingHandler()}>
+                  <span>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                      )}
+                  </span>
+                  <span
+                    {...{
+                      className: header.column.getCanSort()
+                        ? `cursor-pointer select-none ${sortElement(header.column.getIsSorted())}`
+                        : '',
+                    }}
+                  />
+                </div>
+              </th>
+            );
+          })}
+        </tr>
+      ))}
+    </thead>
+  );
+}
+
+export default FixedSizeTHead;
