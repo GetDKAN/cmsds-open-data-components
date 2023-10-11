48e526d92cf60561fd33df4de93ed47bf3a05d1f
Bethany Dunfield <bethany.dunfield@civicactions.com>
2023-10-11 14:22:44 -0600

    Update based on design feedback

--- src/templates/FilteredResource/QueryRow.jsx
+++ src/templates/FilteredResource/QueryRow.jsx
@@ -58,23 +58,22 @@ const QueryRow = ({ id, condition, index, update, remove, propertyOptions, schem
   }, [value]);
 
   return (
-    <fieldset className="ds-u-display--flex ds-u-flex-wrap--wrap ds-u-justify-content--between ds-u-align-items--center">
+    <fieldset className="ds-u-display--flex ds-u-flex-wrap--wrap ds-u-justify-content--between ds-u-md-justify-content--end ds-u-align-items--center">
       <Dropdown
         options={propertyOptions}
         value={property}
         label="Property"
         name={`${condition.key}_property`}
         onChange={(e) => setProperty(e.target.value)}
-        className={"ds-l-md-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2"}
+        className={"ds-l-md-col--5 ds-l-lg-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2 ds-u-margin-bottom--1 ds-u-md-margin-bottom--0"}
       />
       <Dropdown
         options={buildOperatorOptions(schema[id].fields[property].mysql_type)}
         value={operator}
-        size="small"
         label="Operator"
         name={`${condition.key}_operator`}
         onChange={(e) => setOperator(e.target.value)}
-        className={"ds-l-sm-col--3 ds-l-md-col--2 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2"}
+        className={"ds-l-sm-col--3 ds-l-md-col--2 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2 ds-u-margin-bottom--1 ds-u-md-margin-bottom--0"}
       />
       {schema[id].fields[property].mysql_type === 'date' ? (
         <div>
@@ -116,12 +115,12 @@ const QueryRow = ({ id, condition, index, update, remove, propertyOptions, schem
           name={`${condition.key}_value`}
           value={cleanText(value, operator)}
           onChange={(e) => setValue(e.target.value)}
-          className={"ds-l-md-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-sm-padding-right--2"}
+          className={"ds-l-md-col--5 ds-l-lg-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-sm-padding-right--2 ds-u-md-padding-right--0 ds-u-lg-padding-right--2"}
         />
       )}
 
       <Button
-        className="ds-u-margin-top--3 ds-l-col--12 ds-l-md-col--2 ds-l-sm-col--4"
+        className="ds-u-margin-top--3 ds-l-col--12 ds-l-md-col--4 ds-l-lg-col--2 ds-l-sm-col--4"
         aria-label="Delete filter"
         onClick={() => remove(index)}
       >
