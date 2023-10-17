649d33aaef574ca9a72467e7a8641c8b0d975411
Bethany Dunfield <bethany.dunfield@civicactions.com>
2023-10-17 14:15:18 -0600

    remove unneeded class name

--- src/templates/FilteredResource/QueryRow.jsx
+++ src/templates/FilteredResource/QueryRow.jsx
@@ -78,7 +78,7 @@ const QueryRow = ({ id, condition, index, update, remove, propertyOptions, schem
       {schema[id].fields[property].mysql_type === 'date' ? (
         <div className="ds-l-md-col--5 ds-l-lg-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-sm-padding-right--2 ds-u-md-padding-right--0 ds-u-lg-padding-right--2">
           <label
-            className="ds-c-label ds-c-label-margin"
+            className="ds-c-label"
             htmlFor={`${condition.key}_date_value`}
             id={`${condition.key}_date_value-label`}
           >
