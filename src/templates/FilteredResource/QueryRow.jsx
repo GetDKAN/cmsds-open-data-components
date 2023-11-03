9805a89448e949701589e582945e3b63a8594a1b
Bethany Dunfield <bethany.dunfield@civicactions.com>
2023-11-03 10:57:39 -0600

    WCMS-17459: Fix Dropdown render

--- src/templates/FilteredResource/QueryRow.jsx
+++ src/templates/FilteredResource/QueryRow.jsx
@@ -59,22 +59,24 @@ const QueryRow = ({ id, condition, index, update, remove, propertyOptions, schem
 
   return (
     <fieldset className="ds-u-display--flex ds-u-flex-wrap--wrap ds-u-justify-content--between ds-u-md-justify-content--end ds-u-align-items--center">
-      <Dropdown
-        options={propertyOptions}
-        value={property}
-        label="Property"
-        name={`${condition.key}_property`}
-        onChange={(e) => setProperty(e.target.value)}
-        className={"ds-l-md-col--5 ds-l-lg-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2 ds-u-margin-bottom--0 ds-u-md-margin-bottom--2"}
-      />
-      <Dropdown
-        options={buildOperatorOptions(schema[id].fields[property].mysql_type)}
-        value={operator}
-        label="Operator"
-        name={`${condition.key}_operator`}
-        onChange={(e) => setOperator(e.target.value)}
-        className={"ds-l-sm-col--3 ds-l-md-col--2 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2 ds-u-margin-bottom--0 ds-u-md-margin-bottom--2"}
-      />
+      <div className={"ds-l-md-col--5 ds-l-lg-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2 ds-u-margin-bottom--0 ds-u-md-margin-bottom--2"}>
+        <Dropdown
+          options={propertyOptions}
+          value={property}
+          label="Property"
+          name={`${condition.key}_property`}
+          onChange={(e) => setProperty(e.target.value)}
+        />
+      </div>
+      <div className={"ds-l-sm-col--3 ds-l-md-col--2 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2 ds-u-margin-bottom--0 ds-u-md-margin-bottom--2"}>
+        <Dropdown
+          options={buildOperatorOptions(schema[id].fields[property].mysql_type)}
+          value={operator}
+          label="Operator"
+          name={`${condition.key}_operator`}
+          onChange={(e) => setOperator(e.target.value)}
+        />
+      </div>
       {schema[id].fields[property].mysql_type === 'date' ? (
         <div className="ds-l-md-col--5 ds-l-lg-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-sm-padding-right--2 ds-u-md-padding-right--0 ds-u-lg-padding-right--2">
           <label
@@ -110,13 +112,15 @@ const QueryRow = ({ id, condition, index, update, remove, propertyOptions, schem
           />
         </div>
       ) : (
-        <TextField
-          label="Value"
-          name={`${condition.key}_value`}
-          value={cleanText(value, operator)}
-          onChange={(e) => setValue(e.target.value)}
-          className={"ds-l-md-col--5 ds-l-lg-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-sm-padding-right--2 ds-u-md-padding-right--0 ds-u-lg-padding-right--2 ds-u-margin-bottom--2"}
-        />
+        <div className={"ds-l-md-col--5 ds-l-lg-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-sm-padding-right--2 ds-u-md-padding-right--0 ds-u-lg-padding-right--2 ds-u-margin-bottom--2"}>
+          <TextField
+            label="Value"
+            name={`${condition.key}_value`}
+            value={cleanText(value, operator)}
+            onChange={(e) => setValue(e.target.value)}   
+          />
+        </div>
+
       )}
 
       <Button
