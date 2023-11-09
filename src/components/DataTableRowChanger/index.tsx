409ffb1c9116c02151eac7b521f92da025bf045b
Bethany Dunfield <bethany.dunfield@civicactions.com>
2023-11-09 14:57:26 -0700

    WCMS-17327: Fix rows per page display (#148)

---
+++ src/components/DataTableRowChanger/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Dropdown, DropdownValue, DropdownOption } from '@cmsgov/design-system';
+
+type DataTableRowChangerProps = {
+  limit: DropdownValue,
+  rowOptions: Array<Number>,
+  setLimit: Function,
+};
+
+const DataTableRowChanger = (props: DataTableRowChangerProps) => {
+  const {limit, rowOptions, setLimit} = props;
+  const rowOptionsFormatted = rowOptions.map((row: Number) => ({ label: row.toString(), value: row.toString() as DropdownValue } )) as Array<DropdownOption>;
+  return (
+    <div className="ds-u-display--flex">
+      <Dropdown
+        options={rowOptionsFormatted}
+        label="Rows per page:"
+        labelClassName="ds-u-margin-top--0"
+        name="datatable_rows_per_page"
+        onChange={(e) => setLimit(e.target.value)}
+        defaultValue={limit.toString()}
+      />
+    </div>
+  );
+};
+
+DataTableRowChanger.defaultProps = {
+  rowOptions: [10, 25, 50, 100],
+};
+
+DataTableRowChanger.propTypes = {
+  rowOptions: PropTypes.arrayOf(PropTypes.number),
+  setLimit: PropTypes.func.isRequired,
+  limit: PropTypes.number,
+};
+
+export default DataTableRowChanger;
