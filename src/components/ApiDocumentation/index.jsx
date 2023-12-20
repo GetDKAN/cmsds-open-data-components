6eea5a82800d0af0f8062772e8182be3a4586764
Nana Poku <apoku2525@gmail.com>
2023-12-19 20:32:36 -0500

    WCMS-9877: Proposed changes fixes color contrast issues noted in WCMS-6698 and WCMS-8942.

--- src/components/ApiDocumentation/index.jsx
+++ src/components/ApiDocumentation/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
+import '../../templates/APIPage/swagger-ui-overrides.scss';
 
 const ApiDocumentation = ({ endpoint }) => {
   return (
