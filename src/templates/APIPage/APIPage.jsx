c14dd69d14033ce2d4f80f2924fd17de42fb4b47
Bethany Dunfield <bethany.dunfield@civicactions.com>
2024-02-12 13:25:58 -0700

    WCMS-17599: Add horizontal scroll to swagger

--- src/templates/APIPage/APIPage.jsx
+++ src/templates/APIPage/APIPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import qs from 'qs';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
-import './swagger.scss';
 import './swagger-ui-overrides.scss';
 
 const APIPage = ({ hideAuth, additionalParams, rootUrl }) => {
