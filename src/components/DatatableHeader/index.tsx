db925777621a17edd184b5da3e7d5bca6655fd97
Avery Smith <avery@averysmith.pro>
2024-03-28 16:41:29 -0400

    Export CSV button: aria-label doesn't match visible text (#179)
    * Export CSV button: aria-label doesn't match visible text
    
    * corrected failing test
    
    * adjusted button layout
    
    * further adjusted button layout
    
    * added bottom margin to left most button for xs breakpoint and up

--- src/components/DatatableHeader/index.tsx
+++ src/components/DatatableHeader/index.tsx
@@ -10,15 +10,15 @@ const DataTableHeader = ({ resource, downloadURL, jsonUrl } : {resource: Resourc
   return (
     <div className="ds-l-row ds-u-align-items--center ds-u-margin-bottom--2">
       <div className="ds-l-col--12 ds-u-display--flex ds-u-flex-wrap--wrap ds-u-justify-content--between ds-u-align-items--center">
-        <div className="ds-l-col--12 ds-l-lg-col--6">
+        <div className="ds-l-col--12 ds-l-lg-col--4">
           <DataTablePageResults
             totalRows={intCount}
             limit={limit}
             offset={offset}
           />
         </div>
-        <div className="dc-c-resource-header--buttons ds-l-col--12 ds-l-lg-col--6 ds-u-display--flex ds-u-flex-wrap--wrap ds-u-justify-content--end ds-u-margin-bottom--2 ds-u-md-margin-bottom--0 ds-u-padding-x--0">
-          <div className="ds-l-col--12 ds-l-sm-col--auto ds-u-padding-x--0 ds-u-sm-margin-right--2 ds-u-margin-bottom--2 ds-u-sm-margin-bottom--0">
+        <div className="dc-c-resource-header--buttons ds-l-col--12 ds-l-lg-col--8 ds-u-display--flex ds-u-flex-wrap--wrap ds-u-justify-content--end ds-u-margin-bottom--2 ds-u-md-margin-bottom--0 ds-u-padding-x--0">
+          <div className="ds-l-col--12 ds-l-sm-col--auto ds-u-padding-x--0 ds-u-margin-bottom--2 ds-u-md-margin-bottom--0">
             <Tooltip
               onOpen={() => {
                 navigator.clipboard.writeText(window.location.href);
@@ -34,14 +34,13 @@ const DataTableHeader = ({ resource, downloadURL, jsonUrl } : {resource: Resourc
               </span>
             </Tooltip>
           </div>
-          <div className="ds-l-col--12 ds-l-sm-col--auto ds-u-padding-x--0">
+          <div className="ds-l-col--12 ds-l-sm-col--auto ds-u-padding-x--0 ds-u-sm-margin-left--2">
             <Button
               className="ds-u-text-align--center ds-u-font-weight--normal ds-u-font-size--base ds-u-margin-right--1 ds-u-display--inline-block ds-l-col--12"
               href={downloadURL}
-              aria-label="Download filtered data (CSV)"
             >
               <span className="ds-u-font-weight--semibold ds-u-margin-left--1 ds-u-padding--0">
-                <i className="fas fa-file-csv"></i> Export CSV
+                <i className="fas fa-file-csv"></i> Download filtered data (CSV)
               </span>
             </Button>
           </div>
