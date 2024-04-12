bc8fa018640e93433a6972b41cd4a28294c376e1
Bethany Dunfield <bethany.dunfield@civicactions.com>
2024-04-12 14:02:37 -0600

    Restore axios function as part of useMetastoreDataset

--- src/services/useMetastoreDataset/useMetastoreDataset.tsx
+++ src/services/useMetastoreDataset/useMetastoreDataset.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import qs from 'qs';
 import { DatasetType } from '../../types/dataset';
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 const useMetastoreDataset = (datasetId : string, rootAPIUrl : string, additionalParams={}) => {
   const [dataset, setDataset] = useState({
@@ -16,18 +17,16 @@ const useMetastoreDataset = (datasetId : string, rootAPIUrl : string, additional
   const [rootUrl, setRootUrl] = useState(rootAPIUrl)
   const additionalParamsString = Object.keys(additionalParams).length ? `&${qs.stringify(additionalParams)}` : '';
 
-  const {data, isPending, error} = useQuery({
+  const { data, isPending } = useQuery({
     queryKey: ["metastore" + id + additionalParamsString],
     queryFn: () => {
-      return fetch(`${rootUrl}/metastore/schemas/dataset/items/${id}?show-reference-ids${additionalParamsString}`)
-        .then(res => res.json())
-        .then((res) => {
-          return res;
-        })
+      return axios.get(`${rootUrl}/metastore/schemas/dataset/items/${id}?show-reference-ids${additionalParamsString}`)
+        .then((res) => res.data)
+        .catch((error) => {return {title: dataset.title, distribution: dataset.distribution, error: error, description: dataset.description, identifier: dataset.identifier, modified: dataset.modified}});
     }
   })
   useEffect(() => {
-    if (data && data != dataset)
+    if (!isPending && data && data != dataset)
       setDataset(data)
   }, [data])
 
