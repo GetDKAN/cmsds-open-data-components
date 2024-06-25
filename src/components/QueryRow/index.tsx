57c558163b6f40e26c68ff87f4093b44ea0008db
Avery Smith <avery@averysmith.pro>
2024-06-25 09:40:12 -0400

    WCMS-20864: updated component query-row breakpoint layout for mobile and md

--- src/components/QueryRow/index.tsx
+++ src/components/QueryRow/index.tsx
@@ -57,10 +57,10 @@ const QueryRow = ({ id, condition, index, update, remove, propertyOptions, schem
 
   return (
     <fieldset className="ds-u-display--flex ds-u-flex-wrap--wrap ds-u-justify-content--between ds-u-align-items--center ds-u-border--0">
-      <div className="ds-l-col--12 ds-l-md-col--8 ds-u-display--flex ds-u-justify-content--between ds-u-padding-x--0">
+        Test A
         <Dropdown
           options={propertyOptions}
-          className="ds-l-col--8 ds-u-padding-left--0"
+          className="ds-l-md-col--5 ds-l-lg-col--4 ds-l-sm-col--8 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2 ds-u-margin-bottom--0 ds-u-md-margin-bottom--2"
           value={property}
           label="Column Name"
           name={`${condition.key}_property`}
@@ -68,14 +68,13 @@ const QueryRow = ({ id, condition, index, update, remove, propertyOptions, schem
         />
         <Dropdown
           options={buildOperatorOptions(schema[id].fields[property].mysql_type)}
-          className="ds-l-col--4 ds-u-padding-x--0"
+          className="ds-l-sm-col--3 ds-l-md-col--2 ds-l-col--12 ds-u-padding--0 ds-u-md-padding-right--2 ds-u-margin-bottom--0 ds-u-md-margin-bottom--2"
           value={operator}
           label="Operator"
           name={`${condition.key}_operator`}
           onChange={(e) => setOperator(e.target.value)}
         />
-      </div>
-      <div className="ds-l-col--12 ds-l-md-col--4 ds-u-padding-x--0 ds-u-md-padding-left--2 ds-u-display--flex ds-u-justify-content--between">
+      <div className="ds-l-col--12 ds-l-md-col--4 ds-u-padding-x--0 ds-u-md-padding-left--2 ds-u-display--flex ds-u-justify-content--between ds-u-md-margin-bottom--2">
         {schema[id].fields[property].mysql_type === 'date' ? (
           <div>
             <label
