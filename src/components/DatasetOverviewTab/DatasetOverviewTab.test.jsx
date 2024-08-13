0813cdc4042a68669df5bca9875f89330887a224
Avery Smith <avery@averysmith.pro>
2024-08-13 16:18:37 -0400

    WCMS-21904: added drawer functionality to DatasetOverviewTab component

--- src/components/DatasetOverviewTab/DatasetOverviewTab.test.jsx
+++ src/components/DatasetOverviewTab/DatasetOverviewTab.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import DatasetOverview from './index';
 import { defaultMetadataMapping } from '../../assets/metadataMapping';
@@ -30,4 +30,22 @@ describe('<DatasetOverview />', () => {
     expect(screen.getByText("69")).toBeInTheDocument();
     expect(screen.getByText("22")).toBeInTheDocument();
   });
+  it("should render Drawer", () => {
+    const metadataMapping = {
+      ...defaultMetadataMapping,
+    }
+
+    render(
+      <MemoryRouter>
+        <DatasetOverview
+          resource={resource}
+          distributions={distribution.distribution}
+          dataset={dataset}
+          metadataMapping={metadataMapping}
+        />
+      </MemoryRouter>
+    )
+    expect(screen.getByText("Metadata Definitions")).toBeInTheDocument();
+    expect(screen.getByText("What do these fields mean?")).toBeInTheDocument();
+  })
 });
