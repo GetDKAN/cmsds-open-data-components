3dad62178a2911f6c2c73de7ca2868fa9cb35275
Bethany Dunfield <bethany.dunfield@civicactions.com>
2024-08-20 12:10:30 -0600

    WCMS-22016: Don't re-run queries when browser tab is changed

--- src/utilities/QueryProvider/QueryProvider.jsx
+++ src/utilities/QueryProvider/QueryProvider.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+});
 
 const withQueryProvider = (WrappedComponent) => (props) => {
   return (
