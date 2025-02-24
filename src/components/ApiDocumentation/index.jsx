1f1303c0502d55646ceb729519c30d0a7e962915
Daniel Gading <dgading@proton.me>
2025-02-24 16:31:23 -0500

    Update swagger info tags using swagger-ui-layout (#277)

--- src/components/ApiDocumentation/index.jsx
+++ src/components/ApiDocumentation/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
+import { SpanOpenAPIVersion, SpanVersionStamp } from '@civicactions/swagger-ui-layout';
 import 'swagger-ui-react/swagger-ui.css';
 import '../../templates/APIPage/swagger-ui-overrides.scss';
 
 const ApiDocumentation = ({ endpoint }) => {
   return (
     <div>
-      <SwaggerUI url={endpoint} />
+      <SwaggerUI
+        url={endpoint}
+        plugins={[SpanOpenAPIVersion, SpanVersionStamp]}
+      />
     </div>
   );
 };
