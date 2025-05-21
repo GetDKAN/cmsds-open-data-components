ac7513e46e6180e58924c7c7e7cccf550ba92ff7
Avery Smith <avery@averysmith.pro>
2025-05-21 16:44:07 -0400

    WCMS-24611: refactored component to use cms design system

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,142 +1,61 @@
-import React, { useState, useEffect, useContext, useRef } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, FormGroup, Label, Input, InputGroup, InputGroupText } from 'reactstrap'
-import { useMediaQuery } from 'react-responsive'
+import { Button, TextField } from '@cmsgov/design-system'
 import MagnifyingGlassIcon from '../../assets/icons/magnifyingglass'
-import CloseCircledIcon from '../../assets/icons/closecircled'
 import ChevronRightIcon from '../../assets/icons/chevronright'
-import './SearchInput.scss'
+import './search-input.scss'
 
 const SearchInput = ({
-  className = '',
-  labelContent = 'Search',
-  onChangeFunction,
   placeholder = 'Search the Data',
-  value = '',
-  bsSize = 'lg',
-  labelClassName = '',
-  srOnly = true,
-  resetContent = 'Reset',
-  submitContent = 'Submit',
-  showSubmit = true,
-  submitID = 'inputSubmit',
-  defaultSubmit = true,
-  showReset = true,
-  inputClassName = 'ds-c-field',
-  darkTheme = false,
-  submitClassName = '',
-  showMagnifyingGlass = false
-}) => {
-  const [searchQuery, setSearchQuery] = useState(value)
-  const searchInput = useRef(null)
-  const isMobile = useMediaQuery({ minWidth: 0, maxWidth: 768 })
-
-  useEffect(() => {
-    if (defaultSubmit && value === '') {
-      setSearchQuery(value)
-    } else {
-      const readableTerm = value.slice(value.indexOf('=') + 1).split('%20').join(' ')
-      setSearchQuery(readableTerm)
-    }
-  }, [value])
-
-  useEffect(() => {
-    if (defaultSubmit) {
-      const timer = setTimeout(() => {
-        if (searchQuery !== value) {
-          onChangeFunction({ type: 'UPDATE_FULLTEXT', data: { fulltext: searchQuery, page: 1 } })
-        }
-      }, 500)
-
-      return () => clearTimeout(timer)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchQuery, onChangeFunction, defaultSubmit])
-
-  const reset = (
-    <Button
-      type='reset'
-      id='inputReset'
-      aria-label='Clear search input'
-      className='ds-c-button'
-      onClick={() => { setSearchQuery('') }}
-    >
-      <span className='reset-text'>
-        <CloseCircledIcon />
-        <span className='sr-only'>{resetContent}</span>
-      </span>
-    </Button>
-  )
-  const labelClass = srOnly ? 'sr-only' : ''
-  const defaultSubmitClasses = 'ds-c-button ds-c-button--alternate'
-  const darkThemeClasses = 'ds-c-button--on-dark ds-c-button--solid'
-  const combinedSubmitClasses = `${defaultSubmitClasses} ${submitClassName || ''} ${darkTheme ? darkThemeClasses : ''} ${isMobile ? 'mobile' : ''}`.trim()
-
-  return (
-    <FormGroup className={`dc-search-input ${className}`}>
-      <Label for='inputSearch' className={`${labelClass} ${labelClassName}`}>{labelContent}</Label>
-      <InputGroup>
-        {showMagnifyingGlass &&
-        <InputGroupText addon>
-          { !isMobile ? (
-           <MagnifyingGlassIcon />
-          ) : ('') }
-        </InputGroupText> }
-        <Input
-          type='text'
-          name='inputSearch'
-          id='inputSearch'
-          placeholder={placeholder}
-          className={inputClassName}
-          value={searchQuery}
-          onChange={(e) => {
-            setSearchQuery(e.target.value)
-            onChangeFunction && onChangeFunction(e)
-          }}
-          bsSize={bsSize}
-          innerRef={searchInput}
-        />
-      <span id="searchControls">
-      {showReset && searchQuery.length ? reset : null}
-      {showSubmit && 
-        <Button 
-          type='submit'
-          aria-label='Submit search'
-          className={combinedSubmitClasses}
-          id={submitID}>
-          { !isMobile ? (
-            <>
-            {submitContent}
-            <ChevronRightIcon />
-          </>
-          ) : (
-           <MagnifyingGlassIcon />
-          )}
-      </Button>}
-      </span>
-       </InputGroup>
-   </FormGroup>
-  )
-}
+  showMagnifyingGlass,
+  showSearchButton,
+  onDark,
+  onChange,
+  onSubmit,
+  onKeyDown
+}) => (
+  <div className="search-input-container">
+    <TextField
+      className={`${showMagnifyingGlass && 'left-padding'} ${showSearchButton && 'right-padding'}`}
+      label={placeholder}
+      labelClassName="ds-u-visibility--screen-reader"
+      name="text-field-story"
+      placeholder={placeholder}
+      onBlur={() => {}}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+    />
+    {showMagnifyingGlass && (
+      <div className="magnifying-glass-icon ds-u-visibility--hidden ds-u-sm-visibility--visible">
+        <MagnifyingGlassIcon />
+      </div>
+    )}
+    {showSearchButton && (
+      <Button
+        variation="solid"
+        className={onDark && 'on-dark'}
+        onClick={onSubmit}
+      >
+        <span className="magnifying-glass-icon" aria-label="Search the Data">
+          <MagnifyingGlassIcon />
+        </span>
+        <span className="sm-label">
+          Search the Data <ChevronRightIcon />
+        </span>
+      </Button>
+    )}
+    <div className="rounded-border" />
+  </div>
+)
 
 SearchInput.propTypes = {
   placeholder: PropTypes.string,
-  value: PropTypes.string,
-  bsSize: PropTypes.string,
-  labelClassName: PropTypes.string,
-  srOnly: PropTypes.bool,
-  className: PropTypes.string,
-  resetContent: PropTypes.node,
-  submitContent: PropTypes.node,
-  onChangeFunction: PropTypes.func.isRequired,
-  showSubmit: PropTypes.bool,
-  showReset: PropTypes.bool,
-  submitID: PropTypes.string,
-  defaultSubmit: PropTypes.bool,
-  labelContent: PropTypes.string,
-  inputClassName: PropTypes.string,
-  submitClassName: PropTypes.string,
-  darkTheme: PropTypes.bool,
-  showMagnifyingGlass: PropTypes.bool
+  showMagnifyingGlass: PropTypes.bool,
+  showSearchButton: PropTypes.bool,
+  onDark: PropTypes.bool,
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  onKeyDown: PropTypes.func
 }
+
 export default SearchInput
