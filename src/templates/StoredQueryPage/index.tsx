f973867795400a02da4a1ee1ead9a22b8d0a6509
Dan Goodwin <dcgoodwin2112@gmail.com>
2025-06-09 17:10:46 -0400

    Allow query builder to be hidden. Add stored query page template (#290)

---
+++ src/templates/StoredQueryPage/index.tsx
@@ -0,0 +1,82 @@
+import { useEffect } from 'react';
+import DataTableContext from '../Dataset/DataTableContext';
+import DataTableStateWrapper from '../../components/DatasetTableTab/DataTableStateWrapper';
+import qs from 'qs';
+import '../Dataset/dataset.scss';
+import useMetastoreDataset from '../../services/useMetastoreDataset';
+import useDatastore from '../../services/useDatastore';
+import { getFormatType } from '../../utilities/format';
+import { ColumnType, DistributionType, ResourceType } from '../../types/dataset';
+
+export default function StoredQueryPage({
+  id,
+  rootUrl,
+  customColumns,
+  query,
+  distributionIndex = 0,
+  defaultPageSize = 25,
+  disableTableControls = false,
+}: {
+  id: string;
+  rootUrl: string;
+  customColumns?: Array<ColumnType>;
+  query?: string;
+  distributionIndex?: number;
+  defaultPageSize?: number;
+  disableTableControls?: boolean;
+}) {
+  // TODO parse from stored filter
+  const options = {
+    conditions: query
+      ? JSON.parse(query).map(
+          ({ column, operator, ...rest }: { column: string; operator: string }) => ({
+            property: column,
+            operator:
+              operator === 'is'
+                ? '='
+                : operator === 'is not'
+                ? '<>'
+                : operator === 'or'
+                ? 'in'
+                : operator,
+            ...rest,
+          })
+        )
+      : [],
+  };
+
+  const { dataset, isPending } = useMetastoreDataset(id, rootUrl);
+
+  let distribution = {} as DistributionType;
+  let distributions = dataset.distribution ? dataset.distribution : [];
+  if (distributions.length) {
+    distribution = distributions[distributionIndex];
+  }
+
+  const resource = useDatastore('', rootUrl, {
+    ...options,
+    limit: defaultPageSize,
+  }) as ResourceType;
+
+  useEffect(() => {
+    const localFileFormat = getFormatType(distribution);
+    if (localFileFormat === 'csv') {
+      resource.setResource(distribution.identifier);
+    }
+  }, [distribution]);
+
+  return (
+    <DataTableContext.Provider
+      value={{
+        id: id,
+        resource: resource,
+        distribution: distribution,
+        rootUrl: rootUrl,
+        customColumns: customColumns,
+        datasetTableControls: !disableTableControls,
+      }}
+    >
+      <DataTableStateWrapper showQueryBuilder={false} />
+    </DataTableContext.Provider>
+  );
+}
