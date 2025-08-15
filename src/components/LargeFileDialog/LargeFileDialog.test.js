9053a41663548cda8581eedb9cd6a938833ecc8e
Brenton Kelly <brentonkelly1982@gmail.com>
2025-08-15 09:20:02 -0400

    WCMS-25886: Updated unit tests.

--- src/components/LargeFileDialog/LargeFileDialog.test.js
+++ src/components/LargeFileDialog/LargeFileDialog.test.js
@@ -6,14 +6,14 @@ import LargeFileDialog from './index';
 describe("<LargeFileDialog />", () => {
   test('Renders correctly', () => {
     render(<LargeFileDialog downloadUrl="test" />);
-    expect(screen.getByRole('button', { name: "Download Icon Download" }));
+    expect(screen.getByRole('button', { name: "Download" }));
   });
   test('Opens and Closes correctly', () => {
     window.scrollTo = jest.fn();
     
     render(<LargeFileDialog downloadUrl="test" />);
 
-    fireEvent.click(screen.getByRole('button', { name: "Download Icon Download" }));
+    fireEvent.click(screen.getByRole('button', { name: "Download" }));
     expect(screen.getByRole('link', { name: "Yes, download" }));
     
     fireEvent.click(screen.getByRole('button', { name: "Close modal dialog" }));
