f7ea59b2761f550fd6f580c1c360aa1467fbde73
dcgoodwin2112 <dan.goodwin@civicactions.com>
2025-09-25 18:00:46 -0400

    Resolve Row and Column count issue on overview page when a dataset has multiple distributions

--- src/components/Resource/index.tsx
+++ src/components/Resource/index.tsx
@@ -12,7 +12,7 @@ type ResourcePropsType = {
   title: string
 }
 
-const Resource = ({ distributions, resource, title } : ResourcePropsType ) => {
+const Resource = ({ distributions, resource, rootUrl, title } : ResourcePropsType ) => {
   const sm = useMediaQuery({ minWidth: 0, maxWidth: 767 });
   return (
     <div className="ds-u-display--flex ds-u-flex-wrap--wrap">
@@ -47,7 +47,7 @@ const Resource = ({ distributions, resource, title } : ResourcePropsType ) => {
                       <div className="dc-c-metadata-description ds-u-margin--0" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(dist.data.description) }}/>
                     </div>
                   )}
-                  {fileFormat === "csv" && <ResourceInformation resource={resource} />}
+                  {fileFormat === "csv" && <ResourceInformation rootUrl={rootUrl} distribution={dist} />}
                 </li>
               )
             })
--- src/components/ResourceInformation/index.tsx
+++ src/components/ResourceInformation/index.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import { ResourceType } from '../../types/dataset';
+import React, { useContext } from 'react';
+import { DistributionType } from '../../types/dataset';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import qs from 'qs';
+import { acaToParams } from '../../utilities/aca';
+import { ACAContext } from '../../utilities/ACAContext';
 
-const ResourceInformation = ({ resource } : {resource: ResourceType}) => {
-  const { totalRows, totalColumns } = resource;
+const ResourceInformation = ({ distribution, rootUrl } : {distribution: DistributionType, rootUrl: string}) => {
+  const { ACA } = useContext(ACAContext);
+  const { data: resource } = useQuery({
+    queryKey: ['resource-information', distribution.identifier],
+    queryFn: () => axios.get(`${rootUrl}/datastore/query/${distribution.identifier}?${qs.stringify(acaToParams({results: false, schema: false}, ACA))}`).then(res => res.data)
+  });
+
   return (
     <div className="dc-c-resource-info-table ds-l-col--12 ds-u-padding-left--0 ds-u-margin-y--2">
       <h3 className="ds-u-font-size--base ds-text-heading--xl ds-text-heading--xl">About this Resource</h3>
       <div className="ds-u-display--flex ds-u-text-align--center ds-u-justify-content--center ds-u-md-justify-content--start">
         <div className="ds-u-fill--gray-lightest ds-u-radius ds-u-margin-right--1 ds-u-padding--2">
           <div className="ds-u-padding-top--05">Rows</div>
-          <div className="ds-u-font-weight--bold">{totalRows.toLocaleString()}</div>
+            <div className="ds-u-font-weight--bold">{resource?.count?.toLocaleString()}</div>
         </div>
         <div className="ds-u-fill--gray-lightest ds-u-radius ds-u-margin-right--1 ds-u-padding--2">
           <div>
             <div className="ds-u-padding-top--05">Columns</div>
-            <div className="ds-u-font-weight--bold">{totalColumns.toLocaleString()}</div>
+            <div className="ds-u-font-weight--bold">{resource?.query?.properties?.length?.toLocaleString()}</div>
           </div>
         </div>
       </div>
