ec21fb79c0ba9d036f644168aca421c5795e926a
dcgoodwin2112 <dan.goodwin@civicactions.com>
2025-11-25 11:54:40 -0500

    restore deleted mock

---
+++ __mocks__/mockLinks.js
@@ -0,0 +1,147 @@
+const mockLinks = {
+  'topnav': [
+    {
+      'id': 'aboutus',
+      'label': 'About CMS',
+      'url': 'https://www.cms.gov/about-cms',
+      'target': '_top'
+    },
+    {
+      'id': 'newsroom',
+      'label': 'Newsroom',
+      'url': 'https://www.cms.gov/newsroom',
+      'target': '_top'
+    },
+    {
+      'id': 'dataresearch',
+      'label': 'Data & Research',
+      'url': 'https://www.cms.gov/Research-Statistics-Data-and-Systems/Research-Statistics-Data-and-Systems.html',
+      'target': '_top'
+    }
+  ],
+  'main': [
+    {
+      'id': 'datasets',
+      'label': 'Datasets',
+      'url': '/datasets',
+      'target': '_top'
+    },
+    {
+      'id': 'nadac',
+      'label': 'NADAC',
+      'url': '/nadac',
+      'target': '_top'
+    },
+    {
+      'id': 'api',
+      'label': 'API',
+      'url': '/api',
+      'target': '_top'
+    }
+  ],
+  'footerOpenDataToolLinks': [
+    {
+      'id': 'datacms',
+      'label': 'Data.CMS.gov',
+      'url': 'https://data.cms.gov/',
+      'target': '_blank'
+    },
+    {
+      'id': 'openpayments',
+      'label': 'OpenPaymentsData.CMS.gov',
+      'url': 'https://openpaymentsdata.cms.gov/',
+      'target': '_blank'
+    },
+    {
+      'id': 'medicaid',
+      'label': 'Medicaid.gov',
+      'url': 'https://medicaid.gov/'
+    },
+    {
+      'id': 'datamedicaid',
+      'label': 'Data.Medicaid.gov',
+      'url': 'https://data.medicaid.gov/'
+    },
+    {
+      'id': 'datahealthcare',
+      'label': 'Data.Healthcare.gov',
+      'url': 'https://data.healthcare.gov/',
+      'target': '_blank'
+    }
+  ],
+  'footerAdditionalResourcesLinks': [
+    {
+      'id': 'cmscontactinfo',
+      'label': 'CMS contact info',
+      'url': 'https://www.cms.gov/apps/contacts/'
+    },
+    {
+      'id': 'helpwithfile',
+      'label': 'Help with file formats & plug-ins',
+      'url': 'https://www.cms.gov/About-CMS/Agency-Information/Aboutwebsite/Help.html'
+    },
+    {
+      'id': 'privacysettings',
+      'label': 'Privacy Settings',
+      'url': '#',
+      'dataTag': {
+        'name': 'privacy-policy',
+        'value': 'modal-trigger-footer'
+      }
+    },
+    {
+      'id': 'privacypolicy',
+      'label': 'Privacy policy',
+      'url': 'https://www.cms.gov/privacy',
+      'target': '_blank'
+    }
+  ],
+  'footerUtilityLinks': [
+    {
+      'id': 'webpolicies',
+      'label': 'About CMS',
+      'url': 'https://www.cms.gov/about-cms',
+      'target': '_blank'
+    },
+    {
+      'id': 'plainlanguage',
+      'label': 'Plain Language',
+      'url': 'https://www.plainlanguage.gov/',
+      'target': '_blank'
+    },
+    {
+      'id': 'nofearact',
+      'label': 'No Fear Act',
+      'url': 'https://www.cms.gov/about-cms/agency-information/aboutwebsite/nofearact.html',
+      'target': '_blank'
+    },
+    {
+      'id': 'foia',
+      'label': 'Freedom of Information Act',
+      'url': 'https://www.cms.gov/center/freedom-of-information-act-center.html',
+      'target': '_blank'
+    },
+    {
+      'id': 'nondiscrimination',
+      'label': 'Nondiscrimination & Accessibility',
+      'url': 'https://www.cms.gov/about-cms/agency-information/aboutwebsite/cmsnondiscriminationnotice.html',
+      'target': '_blank'
+    },
+    {
+      'id': 'inspectorgeneral',
+      'label': 'Inspector General',
+      'url': 'https://www.oig.hhs.gov',
+      'target': '_blank'
+    }
+  ],
+  'socialMediaLinks': {
+    'twitter': {
+      'url': 'https://twitter.com/CMSGov'
+    },
+    'linkedin': {
+      'url': 'https://www.linkedin.com/company/centers-for-medicare-&-medicaid-services/'
+    }
+  }
+};
+
+export default mockLinks;
