5b83294dd92546febac1c926c076dae66be8686a
Bethany Dunfield <bethany.dunfield@civicactions.com>
2026-02-10 14:21:11 -0700

    WCMS-27492: Implement API tab changes

--- COMPONENTS_INVENTORY.md
+++ COMPONENTS_INVENTORY.md
@@ -135,5 +135,5 @@ This document provides a comprehensive inventory of all components, services, te
 
 ---
 
-*Last updated: January 28, 2026*  
+*Last updated: February 10, 2026*  
 *Repository: [GetDKAN/cmsds-open-data-components](https://github.com/GetDKAN/cmsds-open-data-components)*
--- src/components/ApiDocumentation/index.jsx
+++ src/components/ApiDocumentation/index.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
-import { SpanOpenAPIVersion, SpanVersionStamp, InfoUrlPrefix } from '@civicactions/swagger-ui-layout';
+import { SpanOpenAPIVersion, SpanVersionStamp } from '@civicactions/swagger-ui-layout';
+import ApiDocsInfo from './ApiDocsInfo';
 import 'swagger-ui-react/swagger-ui.css';
 import '../../templates/APIPage/swagger-ui-overrides.scss';
 
 
-const ApiDocumentation = ({ endpoint }) => {
+const ApiDocumentation = ({ endpoint, docsURL, showRowLimitNotice }) => {
+  // this plugin is defined here rather than in swagger-ui-layout so that it can have docsURL and
+  // showRowLimitNotice easily in scope. I couldn't figure out how to pass the plugin these values
+  // another way.
+  const ApiDocsSwaggerUIPlugin = (props) => {
+    return {
+      wrapComponents: {
+        VersionStamp: (Original, system) => () => {
+          const version = system.specSelectors.version();
+          const classList = "version version-stamp version-stamp--span";
+          return (
+            <span className={classList}>
+              { version }
+            </span>
+          );
+        },
+      },
+      components: {
+        info: (props) => <ApiDocsInfo {...props} isDatasetDocs docsURL={docsURL} showRowLimitNotice={showRowLimitNotice} />,
+      },
+      statePlugins: {
+        docs: {
+          selectors: {
+            docsUrl: (state) => state.get('docsUrl')
+          }
+        }
+      }
+    }
+  }
+
   return (
     <div>
       <SwaggerUI
         url={endpoint}
-        plugins={[SpanOpenAPIVersion, SpanVersionStamp, InfoUrlPrefix]}
+        plugins={[SpanOpenAPIVersion, SpanVersionStamp, ApiDocsSwaggerUIPlugin]}
+        docsURL={docsURL}
       />
     </div>
   );
