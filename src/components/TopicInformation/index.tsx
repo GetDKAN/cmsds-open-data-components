7c03499cf6dc205d2989a4371c5f3764ff4712db
Bethany Dunfield <bethany.dunfield@civicactions.com>
2026-02-12 13:03:33 -0700

    WCMS-27215: Update API tab (#357)

--- src/components/TopicInformation/index.tsx
+++ src/components/TopicInformation/index.tsx
@@ -13,7 +13,7 @@ const TopicInformation = ({topicDetails, theme} : {topicDetails: Array<Topic>, t
     <div className="ds-l-col--12">
       <div className="ds-u-margin-y--1 ds-u-font-weight--bold"><img src={`data:image/svg+xml;utf8,${encodeURIComponent(topic[0].topic_icon)}`} alt='' style={{height: '2rem'}}/> {theme[0].data}</div>
       {topicChips.map(chip => (
-        <div className="ds-u-display--inline-block ds-u-fill--primary ds-u-radius--pill ds-u-padding-x--2 ds-u-padding-y--05 ds-u-margin-right--1 ds-u-font-size--sm"><Link to={chip.href} className="ds-u-color--white">{chip.text}</Link></div>
+        <div className="ds-u-display--inline-block ds-u-fill--primary ds-u-radius--pill ds-u-padding-x--2 ds-u-padding-y--05 ds-u-margin-right--1 ds-u-font-size--sm" key={chip.text}><Link to={chip.href} className="ds-u-color--white">{chip.text}</Link></div>
       ))}
     </div>
   )
