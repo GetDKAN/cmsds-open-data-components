4d49e76abdaf3ae8ebab0d050ad5b1cc18cb25ed
Brenton Kelly <brentonkelly1982@gmail.com>
2026-03-05 17:20:17 -0500

    WCMS-27715: Added tag description.

--- src/components/ApiDocsSwaggerUIPlugin/OperationTag.tsx
+++ src/components/ApiDocsSwaggerUIPlugin/OperationTag.tsx
@@ -1,27 +1,35 @@
 import React, { useState } from 'react';
+import type { Map } from 'immutable';
 
 type OperationTagProps = {
   children: React.ReactNode
   tag: string
+  tagObj: Map<string, any>
 }
 
-const OperationTag: React.FC<OperationTagProps> = ({ children, tag, tagObj, getComponent }) => {
+const OperationTag: React.FC<OperationTagProps> = ({ children, tag, tagObj }) => {
   const [show, toggleShow] = useState(true);
-
-  // https://github.com/swagger-api/swagger-ui/blob/1b33a5976d8f5e02d3bc40de1deae3a4a3642e36/src/core/components/operation-tag.jsx
-  const Markdown = getComponent("Markdown", true)
-  const tagDescription = tagObj.getIn(["tagDetails", "description"], null)
+  const tagDescription = tagObj.getIn(["tagDetails", "description"], null) as string | null;
 
   return (
     <div className='OperationTag'>
-      <div className={`toggle-block toggle-block-container${show ? ' open' : ' closed'}`}>
-        <h3 className='toggle-block-title'>
+      <div
+        className={`
+          toggle-block toggle-block-container
+          ${show ? ' open' : ' closed'}
+          ${show ? ' ds-u-padding-bottom--0 ds-u-margin-bottom--4' : ''}
+        `}
+      >
+        <h3
+          className='toggle-block-title ds-u-margin-y--1 ds-u-display--flex ds-u-justify-content--start ds-u-font-size--lg'
+        >
           <button
             type='button'
             onClick={() => {
               toggleShow(!show);
             }}
             aria-expanded={show}
+            className='toggle-block-title-button ds-u-display--flex ds-u-align-items--center ds-u-text-align--left ds-u-fill--transparent ds-u-border--0 ds-u-padding--0'
           >
             {show ? (
               /* Chevron down */
@@ -50,10 +58,21 @@ const OperationTag: React.FC<OperationTagProps> = ({ children, tag, tagObj, getC
                 <path fill="currentColor" d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"></path>
               </svg>
             )}
-            <span className='toggle-block-title-text'>{tag}</span>
+            <span
+              className='toggle-block-title-text ds-u-display--flex ds-u-align-items--center ds-u-padding-left--4'
+            >
+              {tag}
+            </span>
+            {tagDescription ? <span className='toggle-block-title-description ds-u-font-size--sm ds-u-font-weight--normal ds-u-margin-left--2'>{tagDescription}</span> : null}
           </button>
         </h3>
-        <div className={`toggle-block-panel${show ? ' open' : ' closed'}`}>
+        <div
+          className={`
+            toggle-block-panel ds-u-padding-left--4
+            ${show ? ' open' : ' closed'}
+            ${show ? ' ds-u-display--block' : 'ds-u-display--none'}
+          `}
+        >
           {children}
         </div>
       </div>
